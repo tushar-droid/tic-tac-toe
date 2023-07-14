cf5a9d5ce22d5c1c95389c7ae4106df89b17c1ce
tushar-droid <63186251+tushar-droid@users.noreply.github.com>
2023-07-14 01:05:40 -0700

    trial on changing DOM subject to change

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-console.log("Hello world")
+const gameBoard = (() =>{
+    let board = ['','','','','','','','',''];
+    
+    const updateBoard = (index, marker) =>{
+        board[index] =marker;
+    }
+    const getBoard = () => board;
+    const checkIfEmpty = (index) =>{
+        console.log('index recieved: ', index)
+        return board[index] ==='';
+    }
+    return{
+        updateBoard,
+        getBoard,
+        checkIfEmpty,
+    };
+})()
+
+
+const displayController = () =>{
+    const updateBoard = (index) =>{
+        if(gameBoard.checkIfEmpty(index)){
+            const block = document.getElementById(index);
+            block.textContent = 'X';
+        }
+    }
+    return{
+        updateBoard,
+    }
+}
+
+
+const gameFlow = (() =>{
+    const blocks = document.querySelectorAll('.block');
+    const dc = displayController();
+    blocks.forEach(block => {
+        block.addEventListener('click', ()=>{
+            dc.updateBoard(block.id);
+            console.log(gameBoard.getBoard());
+            gameBoard.updateBoard(block.id, 'X');
+            console.log(gameBoard.getBoard());
+        })
+    });
+})();
+
+
+
