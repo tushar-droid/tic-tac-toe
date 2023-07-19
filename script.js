b6767259aa117c9bc68d75efb2bb58e9ee3825f9
tushar-droid <63186251+tushar-droid@users.noreply.github.com>
2023-07-18 21:54:43 -0700

    all done

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const gameBoard = (() =>{
     let board = ['','','','','','','','',''];
     let wincases = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [1,4,8],
-        [2,4,6]
+        [0,1,2],    //straight
+        [3,4,5],    //straight  
+        [6,7,8],    //straight
+        [0,3,6],    //straight  
+        [1,4,7],    //straight  
+        [2,5,8],    //straight  
+        [0,4,8],    //across
+        [2,4,6]     //across
     ];
     const checkWin =(marker) =>{
         let winner = 'none'
@@ -29,16 +29,40 @@ const gameBoard = (() =>{
     const checkIfEmpty = (index) =>{
         return board[index] ==='';
     }
+    const checkSpaces = () =>{
+        if(board.includes(''))
+        {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    const resetGame = () =>{
+        board = ['','','','','','','','',''];
+    }
+
     return{
         updateBoard,
         getBoard,
         checkIfEmpty,
-        checkWin
+        checkWin,
+        checkSpaces,
+        resetGame,
     };
 })()
 
 
 const displayController = () =>{
+    const emptyTable =() =>{
+        const modal = document.querySelector('.modal');
+        modal.style = 'display:none';
+        const blocks = document.querySelectorAll('.block');
+        blocks.forEach(block => {
+            block.textContent = '';
+        });
+    }
     const updateDOM = (index, marker) =>{
         if(gameBoard.checkIfEmpty(index)){
             const block = document.getElementById(index);
@@ -49,16 +73,25 @@ const displayController = () =>{
     }
 
     const AnnounceWinner = (winner) =>{
-        console.log('The Announced winner is: ', winner);
         const modal = document.querySelector('.modal');
         const win_title = document.querySelector('.winning-marker');
         win_title.textContent = `The winner for this round is: ${winner}`;
         modal.style = 'display:flex';
-
     }
+
+    const gameOver = () =>{
+        const modal = document.querySelector('.modal');
+        const win_title = document.querySelector('.winning-marker');
+        win_title.textContent = `It's a Tie`;
+        modal.style = 'display:flex';
+    }
+
+
     return{
         updateDOM,
         AnnounceWinner,
+        gameOver,
+        emptyTable,
     }
 }
 
@@ -78,6 +111,14 @@ const gameFlow = (() =>{
             };
             if(winner!='none'){
                 dc.AnnounceWinner(winner);
+            }
+            if(!gameBoard.checkSpaces()){
+                dc.gameOver();
+                const play_again = document.querySelector('.play-again');
+                play_again.addEventListener('click', () =>{
+                    gameBoard.resetGame();
+                    dc.emptyTable();
+                })
             }
         })
     });
